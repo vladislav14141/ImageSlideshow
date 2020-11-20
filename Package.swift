eb2300f04a550a85abf1fcd3edc15e8b424bf510
Vlad <Vladislav14141@yandex.ru>
2020-11-20 13:20:41 +1000

    Delete AF, KingFIsher

--- Package.swift
+++ Package.swift
@@ -11,19 +11,19 @@ let package = Package(
         .library(
             name: "ImageSlideshow",
             targets: ["ImageSlideshow"]),
-        .library(
-            name: "ImageSlideshow/Alamofire",
-            targets: ["ImageSlideshowAlamofire"]),
+//        .library(
+//            name: "ImageSlideshow/Alamofire",
+//            targets: ["ImageSlideshowAlamofire"]),
         .library(
             name: "ImageSlideshow/SDWebImage",
             targets: ["ImageSlideshowSDWebImage"]),
-        .library(
-            name: "ImageSlideshow/Kingfisher",
-            targets: ["ImageSlideshowKingfisher"])
+//        .library(
+//            name: "ImageSlideshow/Kingfisher",
+//            targets: ["ImageSlideshowKingfisher"])
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.8.0"),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.0.0"),
+//        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.8.0"),
+//        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
     ],
     targets: [
@@ -45,21 +45,21 @@ let package = Package(
                 "Assets/ic_cross_white@2x.png",
                 "Assets/ic_cross_white@3x.png",
             ]),
-        .target(
-            name: "ImageSlideshowAlamofire",
-            dependencies: ["ImageSlideshow", "AlamofireImage"],
-            path: "ImageSlideshow/Classes/InputSources",
-            sources: ["AlamofireSource.swift"]),
+//        .target(
+//            name: "ImageSlideshowAlamofire",
+//            dependencies: ["ImageSlideshow", "AlamofireImage"],
+//            path: "ImageSlideshow/Classes/InputSources",
+//            sources: ["AlamofireSource.swift"]),
         .target(
             name: "ImageSlideshowSDWebImage",
             dependencies: ["ImageSlideshow", "SDWebImage"],
             path: "ImageSlideshow/Classes/InputSources",
             sources: ["SDWebImageSource.swift"]),
-        .target(
-            name: "ImageSlideshowKingfisher",
-            dependencies: ["ImageSlideshow", "Kingfisher"],
-            path: "ImageSlideshow/Classes/InputSources",
-            sources: ["KingfisherSource.swift"])
+//        .target(
+//            name: "ImageSlideshowKingfisher",
+//            dependencies: ["ImageSlideshow", "Kingfisher"],
+//            path: "ImageSlideshow/Classes/InputSources",
+//            sources: ["KingfisherSource.swift"])
     ],
     swiftLanguageVersions: [.v4, .v4_2, .v5]
 )
